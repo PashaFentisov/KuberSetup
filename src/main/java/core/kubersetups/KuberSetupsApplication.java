cc2efe82304301e05a8fccb0d294aa6ba1582129
Pasha Fentisov <pasha16.ua@gmail.com>
2024-12-24 17:37:31 +0200

    Initial commit

---
+++ src/main/java/core/kubersetups/KuberSetupsApplication.java
@@ -0,0 +1,13 @@
+package core.kubersetups;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuberSetupsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KuberSetupsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/core/kubersetups/KuberSetupsApplicationTests.java
@@ -0,0 +1,13 @@
+package core.kubersetups;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuberSetupsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
